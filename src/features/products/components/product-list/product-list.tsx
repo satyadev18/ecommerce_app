eec62150322467d7e6be30f970df8901ef434ba0
satyadev18 <parodyind44@gmail.com>
2023-12-23 23:17:07 +0530

    added products

--- src/features/products/components/product-list/product-list.tsx
+++ src/features/products/components/product-list/product-list.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ProductService } from '../../hooks/product-service'
+import { ProductComponentProps } from '../../types';
+import ProductComponent from '../product/product';
 
 const ProductListComponent = () => {
-  const {fetchProductList,getProductList} = ProductService();
-
-  useEffect(()=>{
+  const { fetchProductList, getProductList } = ProductService();
+  const [productList, setProductList] = useState([])
+  useEffect(() => {
     fetchProductList(10)
-  },[]);
+  }, []);
+
+  useEffect(() => {
+    setProductList(getProductList)
+  }, [getProductList])
 
   console.log(getProductList)
   return (
-    <div>ProductListComponent</div>
+    <div>
+      {
+        productList.length>0 ?
+            productList.map((product: any, index: number) => (
+              <ProductComponent desc={product.description} title={product.title} price={product.price} productImage={product.image} rating={product.rating.rate} key={index} />
+            ))
+          
+          : <p>Data Not Found</p>
+
+      }
+    </div>
   )
 }
 
--- src/features/products/types/index.ts
+++ src/features/products/types/index.ts
@@ -2,4 +2,12 @@ export interface ProductState {
     productList:any[],
 }
 
+export interface ProductComponentProps {
+title :string;
+desc:string;
+productImage:string;
+price:number;
+rating:number;
+}
+
  
